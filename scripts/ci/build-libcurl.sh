7189da7699c8626372ede05f2908e1acada3fcc3
Jay Wu <jay.wu@konghq.com>
2023-12-28 11:09:31 +0800

    fix(curl): revert to use openssl on macos

--- scripts/ci/build-libcurl.sh
+++ scripts/ci/build-libcurl.sh
@@ -134,9 +134,7 @@ fi
 #####
 # ssl
 ####
-if [ "${RUNNER_OS}" == "macOS" ]; then
-  libcurl_args+=("--with-secure-transport")
-elif [ ! -z "$OPENSSL_BUILD_FOLDER" ]; then
+if [ ! -z "$OPENSSL_BUILD_FOLDER" ]; then
   CPPFLAGS="$CPPFLAGS -I$OPENSSL_BUILD_FOLDER/include"
   LDFLAGS="$LDFLAGS -L$OPENSSL_BUILD_FOLDER/lib -Wl,-rpath,$OPENSSL_BUILD_FOLDER/lib"
 
