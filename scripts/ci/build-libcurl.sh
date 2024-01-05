f203a57bce5952094829906179ac9072e7539a73
Jay Wu <jay.wu@konghq.com>
2024-01-05 22:30:55 +0800

    force rebuild on macos
    1. force rebuild libcurl on macos
    2. add three more options for macos

--- scripts/ci/build-libcurl.sh
+++ scripts/ci/build-libcurl.sh
@@ -15,6 +15,11 @@ mkdir -p $2/source
 FORCE_REBUILD=${FORCE_REBUILD:-}
 FORCE_REBUILD_LIBCURL=${FORCE_REBUILD_LIBCURL:-}
 
+# has a cache issue which will skip the rebuild of libcurl
+if [ "${RUNNER_OS}" == "macOS" ]; then
+  FORCE_REBUILD_LIBCURL="true"
+fi
+
 # @TODO We are explicitly checking the static lib
 if [[ -f $build_folder/lib/libcurl.a ]] && [[ -z $FORCE_REBUILD || $FORCE_REBUILD != "true" ]] && [[ -z $FORCE_REBUILD_LIBCURL || $FORCE_REBUILD_LIBCURL != "true" ]]; then
   echo "Skipping rebuild of libcurl because lib file already exists"
@@ -142,6 +147,9 @@ if [ ! -z "$OPENSSL_BUILD_FOLDER" ]; then
   if [ "${RUNNER_OS}" == "macOS" ]; then
     libcurl_args+=("--with-secure-transport")
     libcurl_args+=("--with-default-ssl-backend=openssl")
+    libcurl_args+=("--without-ca-bundle")
+    libcurl_args+=("--without-ca-path")
+    libcurl_args+=("--with-ca-fallback")
   fi
 else
   libcurl_args+=("--without-ssl")
