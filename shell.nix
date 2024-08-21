7a4d3f44f0dddb5d1dbf5098aacf3baa66ed1614
Filipe Freire <livrofubia@gmail.com>
2024-08-21 10:35:19 +0100

    chore: bump to electron 32

--- shell.nix
+++ shell.nix
@@ -3,11 +3,11 @@ with import <nixpkgs> { };
 mkShell {
   nativeBuildInputs = [
     nodejs_20
-    electron_30
+    electron_32
     stdenv.cc.cc.lib
   ];
   LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-  ELECTRON_OVERRIDE_DIST_PATH = "${electron_30}/bin/";
-  ELECTRON_PATH = "${electron_30}/bin/electron";
+  ELECTRON_OVERRIDE_DIST_PATH = "${electron_32}/bin/";
+  ELECTRON_PATH = "${electron_32}/bin/electron";
   ELECTRON_SKIP_BINARY_DOWNLOAD = 1;
 }
