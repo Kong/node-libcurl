e1d5119794bcef9279ba350ba6294f076280272d
Filipe Freire <livrofubia@gmail.com>
2024-08-27 17:51:47 +0100

    update CopyablePersistentTraits

--- src/Easy.cc
+++ src/Easy.cc
@@ -1006,7 +1006,7 @@ int Easy::CbHstsRead(CURL* handle, struct curl_hstsentry* sts, void* userdata) {
 
         auto idxValueAsObject = idxValueChecked.As<v8::Object>();
 
-        Nan::CopyablePersistentTraits<v8::Object>::CopyablePersistent persistentValue;
+        v8::NonCopyablePersistentTraits<v8::Object>::CopyablePersistent persistentValue;
 
         persistentValue.Reset(Nan::GetCurrentContext()->GetIsolate(), idxValueAsObject);
 
